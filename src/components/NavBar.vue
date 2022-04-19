652594d74442d04cb916621bee5a5bd513466ad0
Viktoriia <viktoriia.svisch@devit-team.com>
2022-04-19 01:51:00 +0000

    navbar: now with search icon

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -12,7 +12,20 @@
         >
       </div>
       <Radio :today_shows="today_shows" />
+      <img src="../assets/imgs/search.svg" class="search" @click="searchVis" />
       <div class="more">•••</div>
+    </section>
+    <section
+      id="search_sec"
+      :style="`display: ${hiddenSearch ? 'none' : 'initial'}`"
+    >
+      <input
+        class="input"
+        placeholder="Recherche"
+        type="text"
+        v-model="search"
+        v-on:keyup.enter="sendQuery"
+      />
     </section>
   </nav>
 </template>
@@ -23,6 +36,24 @@ export default {
   props: ["today_shows"],
   components: {
     Radio
+  },
+  data() {
+    return {
+      hiddenSearch: true,
+      search: ""
+    };
+  },
+  methods: {
+    sendQuery() {
+      this.$router.push({ path: `/search/${this.search}` });
+    },
+    searchVis() {
+      if (this.hiddenSearch) {
+        this.hiddenSearch = false;
+      } else {
+        this.hiddenSearch = true;
+      }
+    }
   }
 };
 </script>
@@ -33,6 +64,30 @@ nav {
   top: 0;
   width: 100%;
   z-index: 3;
+  #search_sec {
+    position: absolute;
+    margin: 0 auto;
+    left: 0;
+    right: 0;
+    width: 1200px;
+    .input {
+      position: absolute;
+      right: 0;
+      max-width: 500px;
+      padding: 12px 20px;
+      margin: 8px 0;
+      box-sizing: border-box;
+      font-family: KionaBold;
+      border-radius: 0px;
+      border: 0px;
+      color: white;
+      background-color: #ffffff40;
+      background-image: url("../assets/imgs/play_white.svg");
+      background-repeat: no-repeat;
+      background-size: 30px 30px;
+      background-position: right 10px top 6px;
+    }
+  }
   .nav {
     width: 1200px;
     margin: 0 auto;
@@ -60,6 +115,10 @@ nav {
       background-color: #ddd;
     }
   }
+  .search {
+    margin-left: 10px;
+    cursor: pointer;
+  }
   .more {
     margin: 23px 10px 0px 10px;
     letter-spacing: -4px;
@@ -67,7 +126,7 @@ nav {
     cursor: pointer;
   }
   @media (max-width: 1200px) {
-    .nav {
+    .nav, #search_sec {
       width: 100%;
     }
   }
