a5ba1d6b65df5a43ff891a6df355a2ecff7ea4b1
Viktoriia <viktoriia.svisch@devit-team.com>
2022-04-23 11:31:19 +0000

    podcast: now can link to other podcasts

--- src/components/WidgetPodcast.vue
+++ src/components/WidgetPodcast.vue
@@ -1,7 +1,13 @@
 <template>
-  <section id="podcast" @click="play_podcast">
-    <img :src="podImg" class="podimg" />
-    <span id="title">{{ pod.tag_list }}</span>
+  <section id="podcast">
+    <img :src="podImg" class="podimg" @click="play_podcast" />
+    <span id="title">
+      <span class="genre side" v-for="genre in pod.genres" v-bind:key="genre"
+        ><router-link :to="{ path: '/search/' + genre }">{{
+          genre
+        }}</router-link></span
+      >
+    </span>
   </section>
 </template>
 <script>
@@ -18,9 +24,17 @@ export default {
   methods: {
     play_podcast() {
       alert(this.pod.title);
+      console.log(this.pod.genre);
     }
   },
   beforeMount() {
+    this.pod.genres = this.pod.tag_list.match(/(".*?"|[^"\s]+)+(?=\s*|\s*$)/g);
+    if (this.pod.genres != null) {
+      this.pod.genres = this.pod.genres.map(g => {
+        if (g[0] == '"') return g.substring(1, g.length - 1);
+        return g;
+      });
+    }
     if (!this.pod.artwork_url) {
       this.podImg = this.pod.avatar_url;
     } else {
@@ -34,24 +48,43 @@ export default {
 #podcast {
   position: relative;
   display: grid;
+  background-color: #ffffff40;
   #title {
     width: 192px;
     bottom: 4px;
-    padding: 2px 4px 2px 4px;
+    padding: 5px 4px 5px 4px;
     color: white;
     display: initial;
     cursor: pointer;
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
+    .genre {
+      font-size: 15px;
+      margin-right: 10px;
+      background-color: #00000020;
+      padding: 2px 4px 2px 4px;
+      cursor: pointer;
+      a {
+        color: white;
+        text-decoration: none;
+      }
+      &:hover {
+        background-color: #00000080;
+      }
+    }
+    .side {
+      font-family: Kiona;
+      letter-spacing: -1px;
+    }
   }
   .podimg {
     width: 200px;
     cursor: pointer;
-    opacity: 0.5;
+    opacity: 1;
     &:hover {
       transition: all 0.2s;
-      opacity: 1;
+      opacity: 0.5;
     }
   }
 }
