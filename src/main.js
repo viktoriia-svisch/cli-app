e679d424a2b7e8d099a390ef52e83b7c5bbccbf3
Viktoriia <viktoriia.svisch@devit-team.com>
2023-11-25 13:11:26 +0000

    Merge branch 'feature/gql-google-calendar' into 'master'

--- src/main.js
+++ src/main.js
@@ -19,93 +19,27 @@ fetch(process.env.BASE_URL + "config.json").then(res => {
     }).$mount("#app");
   });
 });
-const googleCalendarUrl =
-  "https:
-const googleCalendarApiKey = "AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs";
-const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+import graph from "@/graphaxios";
 Vue.mixin({
   methods: {
-    stripHtml(infoHtml, index) {
-      return (
-        infoHtml[index] &&
-        infoHtml[index].textContent &&
-        infoHtml[index].textContent.trim()
-      );
-    },
-    async getRecurrences(id) {
-      if (!id) {
-        return;
-      }
-      const response = await fetch(
-        `${googleCalendarUrl}/${id}?` +
-        ["sanitizeHtml=true", "key=" + googleCalendarApiKey].join("&")
-      );
-      const body = await response.json();
-      if (
-        !body?.recurrence ||
-        !body.recurrence[0] ||
-        body.recurrence[0].indexOf("RRULE:FREQ=WEEKLY") === -1
-      ) {
-        return;
-      }
-      const rules = body.recurrence[0].split(";");
-      const interval = rules.find((r) => r.startsWith("INTERVAL="));
-      if (!interval) {
-        return 1;
-      }
-      return Number(interval.replace("INTERVAL=", ""));
-    },
     async getGoogleCalendar(dateStart, dateEnd) {
-      const response = await fetch(
-        `${googleCalendarUrl}?` +
-        [
-          "singleEvents=true",
-          "timeZone=" + timeZone,
-          "maxAttendees=1",
-          "maxResults=250",
-          "sanitizeHtml=true",
-          "timeMin=" + dateStart.toISOString(),
-          "timeMax=" + dateEnd.toISOString(),
-          "key=" + googleCalendarApiKey,
-        ].join("&")
-      );
-      const body = await response.json();
-      if (!body?.items) {
-        return;
-      }
-      let shows = body.items.filter(
-        (item) => item.summary && item.summary.toLowerCase().indexOf("#show") != -1
-      );
-      const parser = new DOMParser();
-      shows = await Promise.all(
-        shows.map(async (show) => {
-          if (!show.description) {
-            return;
+      const graphRes = await graph(
+        this.$config,
+        "GoogleCalendarShows",
+        `query GoogleCalendarShows($start: String!, $end: String!) {
+          GoogleCalendarShows(start: $start, end: $end) {
+            id
+            name
+            starts_at
+            ends_at
+            redundancy
+            genres
+            dj
           }
-          const htmlDescription = parser.parseFromString(
-            show.description.replace(/<br>/g, ""),
-            "text/xml"
-          );
-          const info = htmlDescription.getElementsByTagName("li");
-          if (!info) {
-            return;
-          }
-          return {
-            starts_at: new Date(show.start.dateTime).getTime().toString(),
-            ends_at: new Date(show.end.dateTime).getTime().toString(),
-            name: this.stripHtml(info, 0),
-            dj: this.stripHtml(info, 1),
-            redundancy: await this.getRecurrences(show.recurringEventId),
-            genres:
-              this.stripHtml(info, 2) &&
-              this.stripHtml(info, 2).replace(/#/g, "").split(" "),
-          };
-        })
+        }`,
+        { start: dateStart, end: dateEnd }
       );
-      shows.sort((showA, showB) => {
-        return Number(showA.starts_at) - Number(showB.starts_at);
-      });
-      return shows;
+      return graphRes && graphRes.GoogleCalendarShows;
     }
   },
 })
